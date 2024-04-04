1fb8e4a41638a31a4ca27b33c0a94cc5822b3441
NeilGGopal <neilggopal@gmail.com>
2024-04-03 17:06:06 -0700

    Added a basic Bump Chart

---
+++ mvp-bump-chart/src/BumpChart.tsx
@@ -0,0 +1,60 @@
+import { ResponsiveBump } from '@nivo/bump'
+import data from './assets/data.json'
+import './BumpChart.css'
+
+function BumpChart() {
+
+    return (
+        <>
+        <div id="chart">
+            <ResponsiveBump
+            data={data}
+            colors={{ scheme: 'spectral' }}
+            lineWidth={3}
+            activeLineWidth={6}
+            inactiveLineWidth={3}
+            inactiveOpacity={0.15}
+            pointSize={10}
+            activePointSize={16}
+            inactivePointSize={0}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={3}
+            activePointBorderWidth={3}
+            pointBorderColor={{ from: 'serie.color' }}
+            axisTop={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '',
+                legendPosition: 'middle',
+                legendOffset: -36,
+                truncateTickAt: 0
+            }}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: '',
+                legendPosition: 'middle',
+                legendOffset: 32,
+                truncateTickAt: 0
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: 'ranking',
+                legendPosition: 'middle',
+                legendOffset: -40,
+                truncateTickAt: 0
+            }}
+            isInteractive={true}
+            margin={{ top: 40, right: 100, bottom: 40, left: 60 }}
+            axisRight={null}
+            />
+        </div>
+        </>
+    )
+}
+
+export default BumpChart
